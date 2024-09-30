30d721a6b278bdc1798accb69237d076eb18ab56
nguyenquoctoan160 <115980213+nguyenquoctoan160@users.noreply.github.com>
2024-09-30 10:46:10 +0700

    Ballon and music

--- my-app/src/page/HappyBirthDay/HappyBirthDay.jsx
+++ my-app/src/page/HappyBirthDay/HappyBirthDay.jsx
@@ -2,11 +2,31 @@ import { useState } from "react";
 import "./happyBirthDay.scss";
 import FrontFace from "./frontFace";
 import BackFace from "./backFace";
+import { FaDoorOpen } from "react-icons/fa";
+import { useRef } from "react";
+import music from "./image/happy-birthday-155461.mp3";
 const HappyBirthDay = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const audioRef = useRef(new Audio(music)); // Tạo một tham chiếu đến đối tượng Audio
+
+  // Hàm phát âm thanh
+  const playAudio = () => {
+    audioRef.current.play();
+  };
+
+  // Hàm dừng âm thanh
+  const stopAudio = () => {
+    audioRef.current.pause(); // Dừng phát
+    audioRef.current.currentTime = 0; // Đặt lại thời gian phát về 0
+  };
+
+  // Thiết lập âm thanh tự động lặp lại
+  audioRef.current.loop = true;
   const toggleBook = () => {
     setIsOpen(!isOpen);
+    if (!isOpen) playAudio();
+    else stopAudio();
   };
 
   return (
@@ -23,7 +43,7 @@ const HappyBirthDay = () => {
         <BackFace></BackFace>
       </div>
       <button className="btnHBD" onClick={toggleBook}>
-        Open/Close Book
+        <FaDoorOpen />
       </button>
     </div>
   );
---
+++ my-app/src/page/HappyBirthDay/component/Balloon/Balloon.jsx
@@ -0,0 +1,25 @@
+import "./balloon.scss";
+const Balloons = () => {
+  return (
+    <div className="Balloonscontainer">
+      <div className="balloon">
+        <div>
+          <span>Happy</span>
+        </div>
+        <div>
+          <span>Birth</span>
+        </div>
+        <div>
+          <span>D</span>
+        </div>
+        <div>
+          <span>A</span>
+        </div>
+        <div>
+          <span>Y</span>
+        </div>
+      </div>{" "}
+    </div>
+  );
+};
+export default Balloons;
